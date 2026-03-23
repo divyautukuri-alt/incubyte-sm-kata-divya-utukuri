c18516ee4ceaaa4f38467140723e9eb37ff57742
divyautukuri-alt <divya.utukuri@gmail.com>
2026-03-23 10:42:48 +0530

    Update employee factory to use Faker for dynamic data generation

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :employee do
-    full_name { "MyString" }
-    job_title { "MyString" }
-    country { "MyString" }
-    salary { "9.99" }
+    full_name { Faker::Name.name }
+    job_title { Faker::Job.title }
+    country { ['India', 'United States', 'Canada', 'United Kingdom', 'Germany'].sample }
+    salary { Faker::Number.between(from: 30000, to: 200000) }
   end
 end
